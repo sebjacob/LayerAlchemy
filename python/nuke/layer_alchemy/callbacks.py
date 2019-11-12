1e54528bfc85dbd83773b164bf455b080378f9c9
Sebastien Jacob <sebjacob@hotmail.com>
2019-11-11 23:10:13 -0500

    add indicators to autolabel callback function

--- python/nuke/layer_alchemy/callbacks.py
+++ python/nuke/layer_alchemy/callbacks.py
@@ -53,8 +53,30 @@ def autolabel():
     layerSetKnob = node.knob('layer_set')
     index = int(layerSetKnob.getValue())
     layerSetName = layerSetKnob.enumName(index)
+    node.knob('indicators').setValue(_getIndicatorValue(node))
     if layerSetName:
         return '{name}\n({layerSet})'.format(name=nodeName, layerSet=layerSetName)
     else:
         return nodeName
 
+
+def _getIndicatorValue(node):
+    """
+    simple function to calculate the indicator value for a node
+    :param node: the Nuke node object
+    :type node: :class:`nuke.Node`
+    :return: the integer indicator value
+    :rtype: int
+    """
+    indicators = 0
+    knobs = node.allKnobs()
+    mixKnob = node.knob('mix')
+    if mixKnob and mixKnob.value() != 1:
+        indicators += 16
+    if node.clones():
+        indicators += 8
+    if any(knob.isAnimated() for knob in knobs):
+        indicators += 1
+    if any(knob.hasExpression() for knob in knobs):
+        indicators += 2
+    return indicators
